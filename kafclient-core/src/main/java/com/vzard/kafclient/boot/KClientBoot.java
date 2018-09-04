b3fb4d871f3e0d33591be42c349a3a1cf0cd3c36
vardlloo <vzardlloo@gmail.com>
2018-09-05 00:05:06 +0800

    "art" update project

---
+++ kafclient-core/src/main/java/com/vzard/kafclient/boot/KClientBoot.java
@@ -0,0 +1,12 @@
+package com.vzard.kafclient.boot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+
+@Slf4j
+public class KClientBoot {
+
+    private ApplicationContext applicationContext;
+
+
+}
---
+++ kafclient-core/src/main/java/com/vzard/kafclient/boot/KafkaHandler.java
@@ -0,0 +1,5 @@
+package com.vzard.kafclient.boot;
+
+
+public class KafkaHandler {
+}
